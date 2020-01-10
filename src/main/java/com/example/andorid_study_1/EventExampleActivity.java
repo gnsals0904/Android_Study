d0803f8d8855a6fee8177fadd57864f2abb2e2aa
HunMinKim <gnsals0904@ajou.ac.kr>
2020-01-10 16:26:54 +0900

    study button event handler

---
+++ src/main/java/com/example/andorid_study_1/EventExampleActivity.java
@@ -0,0 +1,29 @@
+package com.example.andorid_study_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class EventExampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_example);
+
+        Button button = (Button)findViewById(R.id.button2);
+        button.setOnClickListener(
+
+                new Button.OnClickListener(){
+                    public  void onClick(View v){
+                        TextView myTextView = (TextView)findViewById(R.id.textView2);
+                        myTextView.setText("Button clicked");
+                    }
+                }
+        );
+
+    }
+}
