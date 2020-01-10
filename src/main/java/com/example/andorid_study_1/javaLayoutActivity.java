44b7f155d3a0bebd4ef47887b24a0dfc5d93b0c9
HunMinKim <gnsals0904@ajou.ac.kr>
2020-01-10 14:16:11 +0900

    change button color and background color

--- src/main/java/com/example/andorid_study_1/javaLayoutActivity.java
+++ src/main/java/com/example/andorid_study_1/javaLayoutActivity.java
@@ -2,6 +2,7 @@ package com.example.andorid_study_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.RelativeLayout;
@@ -12,7 +13,13 @@ public class javaLayoutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Button myButton = new Button(this);
+        myButton.setText("Press Me");
+        myButton.setBackgroundColor(Color.YELLOW);
+        myButton.setTransformationMethod(null);
+
         RelativeLayout myLayout = new RelativeLayout(this);
+        myLayout.setBackgroundColor(Color.BLUE);
+
         myLayout.addView(myButton);
         setContentView(myLayout);
 
