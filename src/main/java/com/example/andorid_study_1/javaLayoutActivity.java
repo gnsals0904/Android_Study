59e429c40aeb433368d71043e251fc30713ae672
HunMinKim <gnsals0904@ajou.ac.kr>
2020-01-10 14:33:24 +0900

    add textfield on screen

--- src/main/java/com/example/andorid_study_1/javaLayoutActivity.java
+++ src/main/java/com/example/andorid_study_1/javaLayoutActivity.java
@@ -6,6 +6,9 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.EditText;
+
+
 
 public class javaLayoutActivity extends AppCompatActivity {
 
@@ -21,12 +24,27 @@ public class javaLayoutActivity extends AppCompatActivity {
         myLayout.setBackgroundColor(Color.BLUE);
 
         RelativeLayout.LayoutParams buttonParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        //two parameter is height and weight
 
         buttonParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
         buttonParams.addRule(RelativeLayout.CENTER_VERTICAL);
 
+
+        EditText myEditText = new EditText(this);
+
+        myButton.setId(R.id.myButtonId);
+        myEditText.setId(R.id.myEditTextId);
+
+        RelativeLayout.LayoutParams textParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+        textParams.addRule(RelativeLayout.ABOVE,myButton.getId());
+        textParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        textParams.setMargins(0,0,0,80);
+
+
         myLayout.addView(myButton,buttonParams);
         // or use this code myButton.setLayoutParams(buttonParams);
+        myLayout.addView(myEditText,textParams);
 
         setContentView(myLayout);
 
